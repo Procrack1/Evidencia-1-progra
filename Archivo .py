a07f3ae619b08591ccd3b686e42ecec4b7bd2022
Procrack1 <93566064+Procrack1@users.noreply.github.com>
2025-09-28 23:06:47 -0600

    Update Archivo .py

--- Archivo .py
+++ Archivo .py
@@ -210,5 +210,5 @@ def menu():
         else:
             print("Opción incorrecta, por favor elige un número del 1 al 6.")
         input("\n(Presiona Enter para continuar)")
-if _name_ == "_main_":
+if __name__ == "_main_":
     menu()
